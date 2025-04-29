591ce764d2397fb9ec7d79fe373160277477c75e
lmaetrix <manaitlorie@g.cjc.edu.ph>
2025-04-29 18:26:52 +0800

    Add files via upload

---
+++ CarHive/src/mainframe/DefaulTableModel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainframe;
+
+/**
+ *
+ * @author MANAIT_CpE121
+ */
+class DefaulTableModel {
+
+    void addRow(Object[] object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
